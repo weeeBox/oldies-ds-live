7d48da94367e6656d884e885a76427087bec6216
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-06-16 19:26:32 +0000

    Добавил проектную базу

---
+++ trunk/DuckstazyLive/DuckstazyLive/PSReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace DuckstazyLive
+{
+    class PSReader : ContentTypeReader<PSContent>
+    {
+        protected override PSContent Read(ContentReader input, PSContent existingInstance)
+        {
+            string data = input.ReadString();
+            return new PSContent(data);
+        }
+    }
+}
