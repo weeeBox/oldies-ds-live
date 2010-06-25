1966169d8e061038163e6583dee671a0b7e4395a
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-06-25 22:36:52 +0000

    Add Primitive.cs

---
+++ DuckstazyLive/DuckstazyLive/graphics/Primitive.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DuckstazyLive.graphics
+{
+    public class Primitive
+    {
+        private VertexPositionColor[] vertices;
+        private short[] indices;
+        private VertexDeclaration vertexDeclaration;
+        private GraphicsDevice device;
+        private PrimitiveType primitiveType;
+        private int primitivesCount;
+
+        public Primitive(GraphicsDevice device, VertexPositionColor[] vertices, short[] indices, PrimitiveType type, int primitivesCount)
+        {
+            this.device = device;
+            this.vertices = vertices;
+            this.indices = indices;
+            this.primitiveType = type;
+            this.primitivesCount = primitivesCount;
+
+            vertexDeclaration = new VertexDeclaration(device, VertexPositionColor.VertexElements);            
+        }
+
+        public void Draw(BasicEffect effect)
+        {
+            GraphicsDevice.VertexDeclaration = vertexDeclaration;
+
+            effect.Begin();
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Begin();
+                GraphicsDevice.DrawUserIndexedPrimitives(primitiveType, vertices, 0, vertices.Length, indices, 0, primitivesCount);
+                pass.End();
+            }
+            effect.End();
+        }       
+
+        protected GraphicsDevice GraphicsDevice
+        {
+            get { return device; }
+        }
+
+        protected VertexPositionColor[] Vertices
+        {
+            get { return vertices; }
+        }
+
+        protected short[] Indices
+        {
+            get { return indices; }
+        }
+    }
+}
