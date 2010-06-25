01f70a413fc650dc5246ddf87c672b3e784d24ee
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-06-25 23:16:22 +0000

    Added constants and classes for ground and grass

--- trunk/DuckstazyLive/DuckstazyLive/env/Background.cs
+++ trunk/DuckstazyLive/DuckstazyLive/env/Background.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using DuckstazyLive.env;
 
 namespace DuckstazyLive
 {
@@ -16,27 +17,23 @@ namespace DuckstazyLive
         private static Color GROUND_LOWER_COLOR = new Color(93, 49, 12);
 
         private GradientRect sky;
-        private GradientRect ground;
+        private GradientRect ground1;
+        private Ground ground;
         
         public Background(float groundHeight)
         {
             float screenWidth = App.Width;
             float screenHeight = App.Height;
             float skyHeight = screenHeight - groundHeight;
-            sky = new GradientRect(0, 0, screenWidth, skyHeight, SKY_UPPER_COLOR, SKY_LOWER_COLOR);
-            ground = new GradientRect(0, skyHeight, screenWidth, groundHeight, GROUND_UPPER_COLOR, GROUND_LOWER_COLOR);            
+            sky = new GradientRect(0, 0, screenWidth, skyHeight, SKY_UPPER_COLOR, SKY_LOWER_COLOR);            
+            ground = new Ground(0, skyHeight, screenWidth, groundHeight);
         }
 
         public void Draw(BasicEffect effect)
         {
-            sky.Draw(effect);
+            sky.Draw(effect);            
             ground.Draw(effect);
-        }
-
-        public void fillGround(Color color)
-        {
-            ground.fillWith(color);
-        }
+        }             
 
         private Application App
         {
