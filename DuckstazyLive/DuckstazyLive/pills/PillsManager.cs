9012f5669240e8969d81cc1ec349973fd3149701
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-07-17 12:34:02 +0000

    Pills iter 0

---
+++ DuckstazyLive/DuckstazyLive/pills/PillsManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics;
+using DuckstazyLive.core.graphics;
+
+namespace DuckstazyLive.pills
+{
+    public class PillsManager : IDisposable
+    {
+        private int maxPillsCount;
+        private int pillsCount;        
+        private Pill[] pills;
+        private List<PillListener> pillListeners;
+
+        public PillsManager(int maxPillsCount)
+        {
+            this.maxPillsCount = maxPillsCount;
+            pills = new Pill[maxPillsCount];
+            for (int pillIndex = 0; pillIndex < maxPillsCount; pillIndex++)
+            {
+                pills[pillIndex] = new Pill(this);
+            }
+            pillListeners = new List<PillListener>();
+        }
+
+        #region Update
+
+        public void Update(float dt)
+        {
+            for (int pillIndex = 0; pillIndex < pillsCount; pillIndex++)
+            {
+                Pill pill = pills[pillIndex];
+                pill.Update(dt);
+            }
+        }               
+
+        #endregion
+
+        #region Lifecycle
+
+        public void AddPill(PillType type, float x, float y)
+        {
+            AddPill(type, x, y, 0.0f, 0.0f);
+        }
+
+        public void AddPill(PillType type, float x, float y, float vx, float vy)
+        {
+            Debug.Assert(pillsCount < maxPillsCount, "Out of pills: " + pillsCount);
+            if (pillsCount == maxPillsCount)
+            {
+                return;
+            }
+
+            pills[pillsCount].Init(type, x, y, vx, vy, 0.0f);
+            pillsCount++;
+        }
+
+        public void RemovePill(int index)
+        {
+            Debug.Assert(index >= 0 && index < pillsCount, index + "<" + pillsCount);
+            pillsCount--;
+
+            Pill pill = pills[index];
+            pills[index] = pills[pillsCount];
+            pills[pillsCount] = pill;
+        }
+
+        public void ClearPills()
+        {
+            pillsCount = 0;
+        }
+
+        #endregion
+
+        #region Listeners
+        public void AddPillListener(PillListener listener)
+        {
+            pillListeners.Add(listener);
+        }
+
+        public void RemovePillListener(PillListener listener)
+        {
+            pillListeners.Remove(listener);
+        }
+
+        public void OnPillAdded(Pill pill)
+        {
+            foreach (PillListener listener in pillListeners)
+            {
+                listener.PillAdded(pill);
+            }
+        }
+
+        public void OnPillRemoved(Pill pill)
+        {
+            foreach (PillListener listener in pillListeners)
+            {
+                listener.PillRemoved(pill);
+            }
+        }
+        #endregion
+
+        #region Drawing
+
+        public void Draw(SpriteBatch batch)
+        {
+            for (int pillIndex = 0; pillIndex < pillsCount; pillIndex++)
+            {
+                pills[pillIndex].Draw(batch);
+            }
+        }
+
+        #endregion
+
+        #region Cleanup
+
+        public void Dispose()
+        {
+            pillListeners.Clear();
+            pillListeners = null;
+
+            for (int pillIndex = 0; pillIndex < pillsCount; pillIndex++)
+            {
+                pills[pillIndex].Dispose();
+                pills[pillIndex] = null;
+            }
+            pills = null;
+        }
+
+        #endregion
+    }
+}
