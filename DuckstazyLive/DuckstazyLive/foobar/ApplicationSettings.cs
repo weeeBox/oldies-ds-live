cffd83f2ab32f468b57a19db26091843a0777aa4
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-09-05 16:25:33 +0000

    Add application settings

---
+++ DuckstazyLive/DuckstazyLive/foobar/ApplicationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuckstazyLive.foobar
+{
+    public struct ApplicationSettings
+    {
+        public int width;
+        public int height;
+        public int maxTimersCount;
+        public int maxPlayersCount;
+    }
+}
