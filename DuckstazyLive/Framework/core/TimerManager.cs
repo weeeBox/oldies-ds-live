67510538888d5fae752a071010cea924d379abd9
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-10-15 04:18:15 +0000

    Add new framework

---
+++ DuckstazyLive/Framework/core/TimerManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework.core
+{
+    public class TimerManager
+    {
+        float appTime;
+        List<Timer> timers = new List<Timer>();
+
+        public void update(float dt)
+        {
+            appTime += dt;
+            fireTimers();
+        }
+
+        private void fireTimers()
+        {            
+            List<Timer> newTimers = new List<Timer>(timers);
+            foreach (Timer timer in newTimers)
+            {
+                if ((appTime - timer.lastFired) > timer.desiredInterval)
+                {
+                    timer.lastFired += timer.desiredInterval;
+                    if ((appTime - timer.lastFired) > timer.desiredInterval)
+                        timer.lastFired = appTime;
+                    timer.internalUpdate();
+                }
+            }
+        }
+
+
+        public void registerTimer(Timer timer)
+        {
+            timers.Add(timer);
+            timer.lastFired = appTime;
+        }
+
+        public void deregisterTimer(Timer timer)
+        {
+            timers.Remove(timer);
+        }
+
+        public float AppTime
+        {
+            get { return appTime; }
+        }
+    }
+}
