74acac2d88505e611f70f51ee7cd9ba1dd870608
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-10-26 14:39:57 +0000

    Add sound transforms

--- trunk/DuckstazyLive/Framework/core/SoundTransform.cs
+++ trunk/DuckstazyLive/Framework/core/SoundTransform.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DuckstazyLive.game
+namespace Framework.core
 {
     public class SoundTransform
     {
+        public static readonly SoundTransform NONE = new SoundTransform();
+
         public float pan;
         public float volume;
 
-        public SoundTransform() : this(1.0f, 0.0f)
+        public SoundTransform()
+            : this(1.0f, 0.0f)
         {
 
         }
 
-        public SoundTransform(float volume) : this(volume, 0.0f)
-        {            
+        public SoundTransform(float volume)
+            : this(volume, 0.0f)
+        {
         }
 
         public SoundTransform(float volume, float pan)
