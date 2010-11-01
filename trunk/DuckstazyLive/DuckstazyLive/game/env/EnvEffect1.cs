38eeba55f1024934fb23744412deb7f80106a75a
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-11-01 19:48:51 +0000

    Add effect1

--- trunk/DuckstazyLive/DuckstazyLive/game/env/EnvEffect1.cs
+++ trunk/DuckstazyLive/DuckstazyLive/game/env/EnvEffect1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DuckstazyLive.app;
 
 namespace DuckstazyLive.game.env
 {
@@ -29,30 +30,36 @@ namespace DuckstazyLive.game.env
 		{
             base.draw(canvas);
 
-            //// Временные переменные.
-            //float x;
-            //bool c = false;
+            // Временные переменные.
+            float x;
+            bool c = false;
             //Graphics gr = shape.graphics;
-			
+
             //gr.clear();
             //gr.lineStyle();
             //gr.beginFill(c1);
             //gr.drawRect(0.0, 0.0, 640.0, 400.0);
             //gr.endFill();
-			
-            //x = -160.0+t;
-            //while(x<400.0)
-            //{
-            //    gr.beginFill(c2);
-            //    gr.moveTo(0.0, x);
-            //    gr.lineTo(640.0, x+40.0);
-            //    gr.lineTo(640.0, x+80.0);
-            //    gr.lineTo(0.0, x+40.0);
-            //    gr.endFill();
-				
-            //    c = !c;
-            //    x+=80.0;
-            //}
+
+            ColorTransform trans = new ColorTransform(c1);
+            DrawMatrix m = new DrawMatrix();
+
+            x = -160.0f + t;
+            while (x < 400.0f)
+            {
+                //gr.beginFill(c2);
+                //gr.moveTo(0.0, x);
+                //gr.lineTo(640.0, x + 40.0);
+                //gr.lineTo(640.0, x + 80.0);
+                //gr.lineTo(0.0, x + 40.0);
+                //gr.endFill();
+
+                m.translate(0, x);
+                canvas.draw(Res.IMG_EFFECT_LINE, m, trans);
+
+                c = !c;
+                x += 80.0f;
+            }
 			
             //canvas.draw(shape);            
 		}
