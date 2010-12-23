ed55d90bd4ac1d9bdf098a30ed1d716992a4902b
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2010-12-23 09:56:02 +0000

    Fix figures

--- DuckstazyLive/DuckstazyLive/DuckstazyLive/game/levels/FigureStage.cs
+++ DuckstazyLive/DuckstazyLive/DuckstazyLive/game/levels/FigureStage.cs
@@ -11,19 +11,19 @@ namespace DuckstazyLive.game.levels
     class FigurePattern
     {
         private byte[] pattern;
-        private int width;
-        private int height;
+        private int cols;
+        private int rows;
 
         public float vx, vy;
         public float x, y;
 
         public float cellWidth, cellHeight;
 
-        public FigurePattern(byte[] pattern, int width, int height)
+        public FigurePattern(byte[] pattern, int cols, int rows)
         {
             this.pattern = pattern;
-            this.width = width;
-            this.height = height;
+            this.cols = cols;
+            this.rows = rows;
 
             cellWidth = 1.6f * Pill.DEFAULT_WIDTH;
             cellHeight = 1.5f * Pill.DEFAULT_HEIGHT;
@@ -31,19 +31,32 @@ namespace DuckstazyLive.game.levels
 
         public byte at(int x, int y)
         {
-            int index = y * width + x;
+            int index = y * cols + x;
             Debug.Assert(index >= 0 && index < pattern.Length);
             return pattern[index];
         }
 
-        public int getWidth()
+        public int getColsCount()
         {
-            return width;
+            return cols;
         }
 
-        public int getHeight()
+        public int getRowsCount()
         {
-            return height;
+            return rows;
+        }
+
+        public int getTotalPills()
+        {
+            int total = 0;
+            for (int i = 0; i < pattern.Length; ++i)
+            {
+                byte pill = pattern[i];
+                if (pill == 1 || pill == 2 || pill == 3)
+                    total++;
+            }
+
+            return total;
         }
     }
 
@@ -51,6 +64,7 @@ namespace DuckstazyLive.game.levels
     {
         private Generator gen;
         private float elapsedTime;
+        private float figureAppearTime;
 
         private FigurePattern duckFigure = new FigurePattern(new byte[] 
         {
@@ -58,10 +72,10 @@ namespace DuckstazyLive.game.levels
             0,0,2,4,2,2,0,0,0,0,0,0,0,0,
             0,0,2,4,2,2,0,0,0,0,0,0,0,0,
             3,3,2,2,2,2,0,2,2,2,2,2,2,0,
-            0,0,0,0,2,2,0,2,2,2,2,2,2,0,
             0,0,0,0,2,2,0,2,4,4,4,4,2,0,
             0,0,0,0,2,2,0,2,2,2,2,2,2,0,
-            0,0,0,0,2,2,0,2,4,4,4,4,2,0,
+            0,0,0,0,2,2,0,2,2,2,2,2,2,0,
+            0,0,0,0,2,2,0,2,4,4,4,4,2,2,
             0,0,0,0,2,2,2,2,2,2,2,2,2,2,
             0,0,0,0,2,2,2,2,2,2,2,2,2,0,
             0,0,0,0,0,0,0,0,3,0,3,0,0,0,
@@ -69,12 +83,16 @@ namespace DuckstazyLive.game.levels
         14, 11);
 
         private int numVisibleLines;
+        private int numPills;
+        private int totalPills;
 
         private Dictionary<int, Setuper> setuperLookup;
 
         public FigureStage() : base(TYPE_BONUS)
         {
-            goalTime = 60.0f;
+            duckFigure.vx = -10.0f; // -18.0f;
+            goalTime = (640 + duckFigure.getColsCount() * duckFigure.cellWidth) / Math.Abs(duckFigure.vx);
+
             setuperLookup = new Dictionary<int, Setuper>();
             PowerSetuper power1 = new PowerSetuper(0.0f, PowerSetuper.POWER1);
             PowerSetuper power2 = new PowerSetuper(0.0f, PowerSetuper.POWER2);
@@ -96,15 +114,17 @@ namespace DuckstazyLive.game.levels
             base.start();
 
             elapsedTime = 0;
-
+            figureAppearTime = 0;
+            
             gen = new Generator();
             gen.regen = false;
-            gen.speed = 7.0f;
+            gen.speed = 8.0f;
 
-            numVisibleLines = 0;                       
+            numVisibleLines = 0;
+            totalPills = numPills = duckFigure.getTotalPills();
                         
-            int duckWidth = duckFigure.getWidth();
-            int duckHeight = duckFigure.getHeight();
+            int duckWidth = duckFigure.getColsCount();
+            int duckHeight = duckFigure.getRowsCount();
 
             duckFigure.x = 640.0f;
             duckFigure.y = 0.5f * (400.0f - duckWidth * Pill.DEFAULT_HEIGHT);            
@@ -116,20 +136,20 @@ namespace DuckstazyLive.game.levels
         public override void update(float dt)
         {
             elapsedTime += dt;
-
-            duckFigure.vx = -20.0f;
+            
             duckFigure.vy = (float)(10 * Math.Sin(3.14 * elapsedTime)); 
             duckFigure.x += duckFigure.vx * dt;
             duckFigure.y += duckFigure.vy * dt;
 
             float dx = 640 - duckFigure.x;
-            int newNumVisibleLines = (int)(dx / duckFigure.cellWidth);
-            Console.WriteLine("dx:" + dx);
-            if (newNumVisibleLines > numVisibleLines && newNumVisibleLines <= duckFigure.getWidth())
+            int newNumVisibleLines = (int)(dx / duckFigure.cellWidth);            
+            if (newNumVisibleLines > numVisibleLines && newNumVisibleLines <= duckFigure.getColsCount())
             {
-                
+                if (numVisibleLines == 1)
+                    figureAppearTime = elapsedTime;
+
                 float px = duckFigure.x + numVisibleLines * duckFigure.cellWidth;
-                for (int j = 0; j < duckFigure.getHeight(); ++j)
+                for (int j = 0; j < duckFigure.getRowsCount(); ++j)
                 {
                     int pillId = duckFigure.at(numVisibleLines, j);
                     if (setuperLookup.ContainsKey(pillId))
@@ -159,6 +179,25 @@ namespace DuckstazyLive.game.levels
                     pill.kill();
                     level.pills.ps.startAcid(pill.x, pill.y);
                 }             
+            }
+            else if ("dead" == msg)
+            {
+                if (pill.type == Pill.POWER)
+                {
+                    numPills--;
+                    Debug.WriteLine(numPills + "/" + totalPills);
+                    if (numPills == 0)
+                    {
+                        if (collected == totalPills)
+                        {
+                            Debug.WriteLine("You win");
+                        }
+                        else
+                        {
+                            Debug.WriteLine("You've lost");
+                        }
+                    }
+                }
             }
         }
     }
