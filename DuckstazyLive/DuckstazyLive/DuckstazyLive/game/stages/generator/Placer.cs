d85292fdbf381ac9ed3d5de45bceca36b2e8bbbb
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2011-01-03 02:10:57 +0000

    Fix snakes

--- DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/generator/Placer.cs
+++ DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/generator/Placer.cs
@@ -5,39 +5,43 @@ using System.Text;
 
 namespace DuckstazyLive.game.levels.generator
 {
-	public class Placer
-	{
-		public float x;
-		public float y;
-		
-		protected Setuper setuper;
-		public Placer(Setuper setuper, float x, float y)
-		{
-			this.x = x;
-			this.y = y;
-			this.setuper = setuper;
-		}
-		
-		public Pill place(Pill pill)
-		{
-			return start(pill);
-		}
-		
-		public Pill placeAvoidHero(Pill pill, float distSqr)
-		{
-			Pills pills = Pills.instance;
-			Pill p = null;
-			
-			if(distSqr<=0 || !pills.tooCloseHero(x, y, distSqr))
-				p = start(pill);
-			
-			return p;
-		}
+    public class Placer
+    {
+        public float x;
+        public float y;
+
+        protected Setuper setuper;
+        public Placer(Setuper setuper, float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+            this.setuper = setuper;
+        }
+
+        public Pill place(Pill pill)
+        {
+            return start(pill);
+        }
+
+        public Pill placeAvoidHero(Pill pill, float distSqr)
+        {
+            Pill p = null;
+
+            if (distSqr <= 0 || !getPills().tooCloseHero(x, y, distSqr))
+                p = start(pill);
+
+            return p;
+        }
 
         protected virtual Pill start(Pill pill)
         {
             return setuper.start(x, y, pill);
         }
-	}
+
+        protected Pills getPills()
+        {
+            return GameMgr.getInstance().getPills();
+        }
+    }
 
 }
