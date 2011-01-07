7c60366645b7639e5a2b87f961da40bd3fa291be
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2011-01-07 17:43:55 +0000

    Fix fireworks

--- trunk/DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/story/Fireworks.cs
+++ trunk/DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/story/Fireworks.cs
@@ -81,20 +81,20 @@ namespace DuckstazyLive.game.stages.story
                 startX = 160.0f - Hero.duck_w;
                 pumpFireworkData = new FireworkInfo[]
                 {
-                    new FireworkInfo(FireworkSetuper.POWER1, 0, 380, 480, 200, 2.5f, 120.0f, 8.0f, 3.0f, 10, 0),
-                    new FireworkInfo(FireworkSetuper.POWER2, 640, 380, 160, 200, 2.0f, 150.0f, 8.0f, 1.0f, 9, 0),
-                    new FireworkInfo(FireworkSetuper.POWER3, 320, 250, 320, 80, 1.0f, 160.0f, 4.0f, 1.0f, 9, 0),                    
+                    new FireworkInfo(FireworkSetuper.POWER1, 0, 380, 480, 200, 2.5f, 120.0f, 5.5f, 3.0f, 9, 0),
+                    new FireworkInfo(FireworkSetuper.POWER2, 640, 380, 160, 200, 2.0f, 150.0f, 5.5f, 1.0f, 9, 0),
+                    new FireworkInfo(FireworkSetuper.POWER3, 320, 250, 320, 80, 1.0f, 160.0f, 5.0f, 1.0f, 9, 0),                    
                 };
                 powerFireworkData = new FireworkInfo[]
                 {
-                    new FireworkInfo(FireworkSetuper.POWER1, 0, 380, 320, 80, 2.0f, 200.0f, 4.0f, 0.0f, 8, 1),
-                    new FireworkInfo(FireworkSetuper.POWER1, 640, 380, 320, 80, 2.0f, 200.0f, 4.0f, 0.0f, 7, 1),
-                    new FireworkInfo(FireworkSetuper.POWERS, 640, 80, 160, 250, 1.5f, 200.0f, 3.5f, 0.0f, 6, 1),
-                    new FireworkInfo(FireworkSetuper.POWERS, 0, 80, 480, 250, 1.5f, 200.0f, 3.5f, 0.0f, 6, 1),                    
+                    new FireworkInfo(FireworkSetuper.POWER1, 0, 380, 320, 80, 2.0f, 150.0f, 5.5f, 0.0f, 10, 1),
+                    new FireworkInfo(FireworkSetuper.POWER1, 640, 380, 320, 80, 2.0f, 150.0f, 5.5f, 0.0f, 10, 1),
+                    new FireworkInfo(FireworkSetuper.POWERS, 640, 80, 160, 250, 1.5f, 150.0f, 5.5f, 0.0f, 10, 1),
+                    new FireworkInfo(FireworkSetuper.POWERS, 0, 80, 480, 250, 1.5f, 150.0f, 5.5f, 0.0f, 10, 1),                    
                 };
                 penaltyFireworkData = new FireworkInfo[]
                 {
-                    new FireworkInfo(FireworkSetuper.TOXIC, 320, 80, 320, 100, 0.5f, 150.0f, 5.5f, 0.0f, 12, 0)
+                    new FireworkInfo(FireworkSetuper.TOXIC, 320, 80, 320, 100, 0.5f, 120.0f, 5.5f, 0.0f, 10, 5)
                 };
                 powerEndFireworkData = new FireworkInfo[]
                 {
@@ -119,7 +119,7 @@ namespace DuckstazyLive.game.stages.story
 
             jumpGen = new Generator();
             startJumps();
-            
+
             startFirework();
         }        
 
