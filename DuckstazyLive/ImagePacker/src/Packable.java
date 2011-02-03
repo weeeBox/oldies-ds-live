faf809486c428d9165e772bec0e2a7609cfc9a96
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2011-02-03 15:33:17 +0000

    Add ImagePacker

---
+++ DuckstazyLive/ImagePacker/src/Packable.java
@@ -0,0 +1,11 @@
+public interface Packable 
+{
+	public int getX();
+	public int getY();
+
+	public void setX(int x);
+	public void setY(int y);
+
+	public int getWidth();
+	public int getHeight();
+}
