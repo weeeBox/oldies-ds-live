be07378195838c11f123ad8c3bd824a6df7c0d18
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2011-02-04 17:10:14 +0000

    Fix atlas drawing

--- trunk/DuckstazyLive/Framework/visual/AtlasImage.cs
+++ trunk/DuckstazyLive/Framework/visual/AtlasImage.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Framework.core;
 
 namespace Framework.visual
 {
     public class AtlasImage : SpriteTexture
     {
+        private static Vector2 VECTOR_ZERO = Vector2.Zero;
+        private static Vector2 VECTOR_ONE = new Vector2(1.0f, 1.0f);
+        private static Color COLOR_WHITE = Color.White;
+
         private Atlas atlas;
         private Rectangle quad;
         private int ox;
@@ -31,12 +37,48 @@ namespace Framework.visual
             get { return quad.Height; }
         }
 
-        public void draw(float x, float y) { }
-        public void draw(float x, float y, float opacity) { }
-        public void draw(float x, float y, ref Color color) { }
-        public void draw(ref Vector2 position, ref Color color, float rotation, ref Vector2 origin, ref Vector2 scale, ref Vector2 flip) { }
+        public void draw(float x, float y) 
+        {
+            Texture2D texture = atlas.getTexture();
+            draw(x, y, ref COLOR_WHITE);
+        }
 
-        public void drawPart(ref Rectangle rectangle, float x, float y) { }
-        public void drawTiled(ref Rectangle src, ref Rectangle dst) { }
+        public void draw(float x, float y, float opacity) 
+        {
+            Color color = Color.White * opacity;
+            draw(x, y, ref color);
+        }
+
+        public void draw(float x, float y, ref Color color) 
+        {
+            Texture2D texture = atlas.getTexture();
+            Vector2 position = new Vector2(x, y);
+            AppGraphics.DrawImage(texture, ref quad, ref position, ref color, 0.0f, ref VECTOR_ZERO, ref VECTOR_ONE, ref VECTOR_ZERO);
+        }
+
+        public void draw(ref Vector2 position, ref Color color, float rotation, ref Vector2 origin, ref Vector2 scale, ref Vector2 flip) 
+        {
+            Texture2D texture = atlas.getTexture();
+            AppGraphics.DrawImage(texture, ref quad, ref position, ref color, rotation, ref origin, ref scale, ref flip);
+        }
+
+        public void drawPart(ref Rectangle rectangle, float x, float y) 
+        {
+            Texture2D texture = atlas.getTexture();
+            Vector2 position = new Vector2(x, y);
+            Rectangle partQuad = rectangle;
+            partQuad.X += quad.X;
+            partQuad.Y += quad.Y;
+            AppGraphics.DrawImage(texture, ref partQuad, ref position, ref COLOR_WHITE, 0.0f, ref VECTOR_ZERO, ref VECTOR_ONE, ref VECTOR_ZERO);
+        }
+
+        public void drawTiled(ref Rectangle src, ref Rectangle dst) 
+        {
+            Texture2D texture = atlas.getTexture();
+            Rectangle partSrc = src;
+            partSrc.X += quad.X;
+            partSrc.Y += quad.Y;
+            AppGraphics.DrawImageTiled(texture, ref partSrc, ref dst);
+        }
     }
 }
