173d3506e34d2506bc1da53cd0217f5ddcffec2c
a.lementuev <a.lementuev@1d1e7c73-9c41-3dba-1107-1dd6c39a4caa>
2011-02-15 20:19:37 +0000

    Tutorial progress

--- trunk/DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/story/SingleTutorialStage.cs
+++ trunk/DuckstazyLive/DuckstazyLive/DuckstazyLive/game/stages/story/SingleTutorialStage.cs
@@ -5,21 +5,36 @@ using System.Text;
 using Framework.visual;
 using Framework.core;
 using DuckstazyLive.app;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace DuckstazyLive.game.stages.story
 {
     public abstract class SingleTutorialStep : BaseElementContainer
     {
-        public SingleTutorialStep()
+        protected SingleTutorialStage stage;
+
+        public SingleTutorialStep(SingleTutorialStage stage)
         {
+            this.stage = stage;
             this.width = Constants.SCREEN_WIDTH;
             this.height = (int)Constants.ENV_HEIGHT;
+        }
+
+        public override bool buttonPressed(ref ButtonEvent evt)
+        {
+            return false;
+        }
+
+        public override bool buttonReleased(ref ButtonEvent evt)
+        {
+            return false;
         }
     }
 
     public class GreetingStep : SingleTutorialStep
     {
-        public GreetingStep()
+        public GreetingStep(SingleTutorialStage stage) : base(stage)
         {
             Font font = Application.sharedResourceMgr.getFont(Res.FNT_BIG);
             Text message = new Text(font);
@@ -34,28 +49,99 @@ namespace DuckstazyLive.game.stages.story
             attachHor(buttons, AttachStyle.CENTER);
             UiLayout.attachVert(buttons, message, this, AttachStyle.CENTER);
         }
+
+        public override bool buttonPressed(ref ButtonEvent evt)
+        {
+            if (evt.button == Buttons.A)
+            {
+                stage.nextStep();
+                return true;
+            }
+
+            if (evt.button == Buttons.B)
+            {
+                stage.finish();
+                return true;
+            }
+
+            return base.buttonPressed(ref evt);
+        }
+
+        public override bool buttonReleased(ref ButtonEvent evt)
+        {
+            if (evt.button == Buttons.A || evt.button == Buttons.B)
+                return true;
+
+            return base.buttonReleased(ref evt);
+        }
     }
 
     public class SingleTutorialStage : StoryLevelStage
     {
-        private int TUTORIAL_STEP_GREETINGS = 0;
-        private int TUTORIAL_STEP_MOVE = 1;
-
-        SingleTutorialStep step;
+        private SingleTutorialStep[] steps;
+        private int stepIndex;
 
         public SingleTutorialStage()
         {
-            step = new GreetingStep();            
+            steps = new SingleTutorialStep[] 
+            {
+                new GreetingStep(this)
+            };
         }
-
+        
         protected override void startProgress()
         {
             progress.start(0, 0);
         }
 
+        public override void update(float dt)
+        {
+            base.update(dt);
+            getCurrentStep().update(dt);
+        }
+
         public override void draw2(Canvas canvas)
         {
-            step.draw();
+            getCurrentStep().draw();
+        }
+
+        public override bool buttonPressed(ref ButtonEvent e)
+        {
+            if (getCurrentStep().buttonPressed(ref e))
+                return true;
+
+            return base.buttonPressed(ref e);
+        }
+
+        public override bool buttonReleased(ref ButtonEvent e)
+        {
+            if (getCurrentStep().buttonReleased(ref e))
+                return true;
+
+            return base.buttonReleased(ref e);
+        }
+
+        public void nextStep()
+        {
+            stepIndex++;
+            if (stepIndex == steps.Length)
+            {
+                finish();
+            }
+        }
+
+        public void finish()
+        {
+            win();
+
+            StoryLevel level = (StoryLevel)Level.instance;
+            level.nextLevel();
+        }
+
+        private SingleTutorialStep getCurrentStep()
+        {
+            Debug.Assert(stepIndex >= 0 && stepIndex < steps.Length);
+            return steps[stepIndex];
         }
     }
 }
